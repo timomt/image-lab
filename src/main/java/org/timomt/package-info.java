d885e01010d8945eb95502842a30ed497f011ff2
timomt <119742190+timomt@users.noreply.github.com>
2024-06-14 16:45:00 +0200

    created package-info.java

---
+++ src/main/java/org/timomt/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * An Application to visualize how matrix multiplication can be used to alter image properties like:
+ * <li>colour spectrum</li>
+ * <li>pixel coordinates</li>
+ */
+package org.timomt;
